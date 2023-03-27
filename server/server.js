be70a6617a55510f706968c04fe77815eb76cb8b
waiphy0aung <bgyi354@gmail.com>
2023-03-27 08:25:36 +0630

    first

---
+++ server/server.js
@@ -0,0 +1,16 @@
+import express from "express"
+import path from "path"
+import dotenv from "dotenv"
+import { fileURLToPath } from "url";
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const publicPath = path.join(__dirname,"/../public")
+
+dotenv.config()
+app.use(express.static(publicPath))
+
+app.listen(process.env.PORT,() => {
+  console.log("server running on "+process.env.PORT)
+})
