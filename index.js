bb2b06506092813b3884565556b2a58175acfa2a
waiphy0aung <bgyi354@gmail.com>
2023-03-27 10:08:40 +0630

    update

--- index.js
+++ index.js
@@ -2,18 +2,30 @@ import express from "express"
 import path from "path"
 import dotenv from "dotenv"
 import { fileURLToPath } from "url";
+import http from "http"
+import { Server } from "socket.io"
 
 const app = express();
 const router = express.Router()
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-const publicPath = path.join(__dirname,"/public")
+const publicPath = path.join(__dirname, "/public")
 
 dotenv.config()
 app.use(express.static(publicPath))
+const server = http.createServer(app)
+const io = new Server(server)
 
-app.listen(process.env.PORT,() => {
-  console.log("server running on "+process.env.PORT)
+io.on("connection", (socket) => {
+  console.log("A new user just connected")
+  socket.on("disconnect", () => {
+    console.log("User was disconnected")
+  })
 })
 
-app.use("/.netlify/functions/api",router)
+
+server.listen(process.env.PORT, () => {
+  console.log("server running on " + process.env.PORT)
+})
+
+app.use("/.netlify/functions/api", router)
