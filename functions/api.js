0b66b59ae6f165ff900ef83465750d877e68521f
waiphy0aung <bgyi354@gmail.com>
2023-03-27 08:46:45 +0630

    update

--- functions/api.js
+++ functions/api.js
@@ -1,12 +1,14 @@
 import express from "express"
 import path from "path"
+import serverless from 'serverless-http'
 import dotenv from "dotenv"
 import { fileURLToPath } from "url";
 
 const app = express();
+const router = express.Router()
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-const publicPath = path.join(__dirname,"/../public")
+const publicPath = path.join(__dirname,"/../dist")
 
 dotenv.config()
 app.use(express.static(publicPath))
@@ -14,3 +16,7 @@ app.use(express.static(publicPath))
 app.listen(process.env.PORT,() => {
   console.log("server running on "+process.env.PORT)
 })
+
+app.use("/.netlify/functions/api",router)
+const handler = serverless(app)
+export default handler
